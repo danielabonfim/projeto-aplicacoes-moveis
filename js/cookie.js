2b67b35b71ce395ad235f54d195b4a9f6feed44b
Dani Bonfim <daniela@superopa.com>
2023-05-14 18:30:44 -0300

    displaying fortune

--- js/cookie.js
+++ js/cookie.js
@@ -1,5 +1,31 @@
 const FC_BTN = "#fortune_cookie";
 
+class FortuneList {
+  constructor(fortuneList) {
+    this.fortuneList = fortuneList;
+  }
+
+  toJson() {
+    const lis = [];
+    this.fortuneList.forEach(fortune => lis.push(fortune.toJson()))
+  }
+}
+
+class Fortune {
+  constructor(id, text) {
+    this.id = id;
+    this.text = text;
+  }
+
+  toJson() {
+    return {
+      "id": this.id,
+      "text": this.text
+    }
+  }
+}
+
+
 async function getFortune(){
   try{
     let response = await axios({
@@ -7,52 +33,41 @@ async function getFortune(){
       url: "https://api.adviceslip.com/advice",
       }
     );
-
-    return response.data;
-
+    console.log(response)
+    return  new Fortune(response.data.slip.id, response.data.slip.advice,);
   }catch(e){
     console.log(e);
   };
 
 }
 
-function nextState(){
+async function nextState(){
   const spawned = "spawned";
   const opened = "opened";
 
   // open cookie
   if ($(FC_BTN).hasClass(spawned)) {
+    const fortune = await getFortune();
+    $(".fc-fortune-text").html(fortune.text);
+
+
     $(FC_BTN).removeClass(spawned);
-            $('.fc-fortune').removeClass('d-none')
+    $('.fc-fortune').removeClass('d-none')
     $(FC_BTN).addClass(opened);
+
 
   // new cookie
   } else {
     $(FC_BTN).removeClass(opened);
      $('.fc-fortune').addClass('d-none')
     $(FC_BTN).addClass(spawned);
-
-    getFortune();
   }
 };
 
 
 addEventListener("load",app);
 function app() {
-  
-  const fortuneText = $(".fc-fortune-text");
-
-		// getFortune = function(){
-		// 	fortuneText.innerHTML = "blablablabla";
-		// },
-
-	
-	getFortune();
-
   $(FC_BTN).on("click", ()=>{
     nextState();
-    functionGetAdvice();
   })
-  
-	// FC_BTN.addEventListener("click",nextState);
 }
