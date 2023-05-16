021c1bc65f1312c2c89f4de9976674f58979b367
Dani Bonfim <daniela@superopa.com>
2023-05-16 13:53:24 -0300

    adding no msg

--- js/cookie.js
+++ js/cookie.js
@@ -1,36 +1,19 @@
 const FC_BTN = "#fortune_cookie";
 let currentFortune;
 
-class FortuneList {
-  constructor(fortuneList) {
-    this.fortuneList = fortuneList;
-  }
-
+class FortuneListService {
   toJson() {
     const lis = [];
     this.fortuneList.forEach(fortune => lis.push(fortune.toJson()))
   }
-}
 
-class Fortune {
-  constructor(id, text, timestamp) {
-    this.id = id;
-    this.text = text;
-    this.timestamp = timestamp
-  }
+  removeFortune(timestamp){
+    const localStorageList = localStorage.getItem(this.fortuneList)
+    let fortuneList = localStorageList ? JSON.parse(localStorageList) : [];
 
-  toJson() {
-    return {
-      "id": this.id,
-      "text": this.text,
-      "timestamp": this.timestamp
-    }
-  }
-}
-
-class LocalStorage{
-  constructor(){
-    this.fortuneList = "fortuneList";
+    const index = fortuneList.findIndex((fortune) => fortune.timestamp === timestamp);
+    fortuneList = fortuneList.splice(index, 1);
+    localStorage.setItem(this.fortuneList, JSON.stringify(fortuneList));
   }
 
   getFortuneList(){
@@ -55,17 +38,23 @@ class LocalStorage{
     fortuneList.push(fortune.toJson());
     localStorage.setItem(this.fortuneList, JSON.stringify(fortuneList));
   }
-
-  removeFortune(timestamp){
-    const localStorageList = localStorage.getItem(this.fortuneList)
-    let fortuneList = localStorageList ? JSON.parse(localStorageList) : [];
-
-    const index = fortuneList.findIndex((fortune) => fortune.timestamp === timestamp);
-    fortuneList = fortuneList.splice(index, 1);
-    localStorage.setItem(this.fortuneList, JSON.stringify(fortuneList));
-  }
 }
 
+class Fortune {
+  constructor(id, text, timestamp) {
+    this.id = id;
+    this.text = text;
+    this.timestamp = timestamp
+  }
+
+  toJson() {
+    return {
+      "id": this.id,
+      "text": this.text,
+      "timestamp": this.timestamp
+    }
+  }
+}
 
 async function getFortune(){
   try{
@@ -106,30 +95,41 @@ async function nextState(){
   }
 };
 
-function renderList(localStorage){
-  const fortuneList = localStorage.getFortuneList();
-
-  fortuneList.forEach(fortune =>{
-    $("#fortune_list").append(
-      `
-      <li class="list-group-item d-flex justify-content-between align-items-start">
-      <div class="ms-2 me-auto">
-        <div class="fw-bold">${fortune.timestamp}</div>
-        ${fortune.text}
-      </div>
-      <img class="btn-icon-secondary" src = "trash-can.svg"/>
-     
+function renderList(fortuneListService){
+  const fortuneList = fortuneListService.getFortuneList();
+  let html = ""
+  if(!fortuneList &&  !fortuneList.length){
+    html = `
+    <li id="no_fortune_msg"class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      Você não tem sortes salvas :(
+    </div>
     </li>
     `
-    )
-  })
+  }else{
+    fortuneList.forEach(fortune =>{
+      html +=
+        `
+        <li class="list-group-item d-flex justify-content-between align-items-start">
+        <div class="ms-2 me-auto">
+          <div class="fw-bold">${fortune.timestamp}</div>
+          ${fortune.text}
+        </div>
+        <img class="btn-icon-secondary" src = "trash-can.svg"/>
+       
+      </li>
+      `
+    })
+  }
+
+  $("#fortune_list").html(html);
 }
 
 
 addEventListener("load",app);
 
 function app() {
-  const lc = new LocalStorage();
+  const fortuneListService = new FortuneListService();
 
   $(".btn-secondary").on(
     "mouseenter",
@@ -143,15 +143,31 @@ function app() {
     nextState();
   })
 
-  $("#save_fortune").on("click", ()=> lc.addFortuneToList(currentFortune))
+  function toggleCookieAndList(){
+    if( $("#fortune_list").hasClass("d-none")){
+      $("#fortune_list").removeClass("d-none");
+      $("#fortune_cookie").addClass("d-none");
+      
+      $("#save_fortune").addClass("d-none");
+      $("#back_to_cookie").removeClass("d-none");
+    }else{
+      $("#fortune_list").addClass("d-none");
+      $("#fortune_cookie").removeClass("d-none");
+      
+      $("#save_fortune").removeClass("d-none");
+      $("#back_to_cookie").addClass("d-none");
+    }
+  }
+
+  $("#save_fortune").on("click", ()=> fortuneListService.addFortuneToList(currentFortune))
   $("#saved_list").on("click", ()=>{
     $("#saved_list").addClass("selected");
-    renderList(lc);
-    $("#fortune_list").removeClass("d-none");
-    $("#fortune_cookie").addClass("d-none");
+    renderList(fortuneListService);
     
-    $("#save_fortune").addClass("d-none");
-    $("#back_to_cookie").removeClass("d-none");
+    toggleCookieAndList();
+
+    $("#back_to_cookie").off();
+    $("#back_to_cookie").on("click", toggleCookieAndList);
   })
 
   // $("#delete").on("click", (e)=> {
